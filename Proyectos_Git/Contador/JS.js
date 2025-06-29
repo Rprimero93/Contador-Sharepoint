e203c87fc8cc1376f534b97858890b4ef68df7d5
Rprimero93 <villamil76@gmail.com>
2025-06-28 22:27:23 -0500

    agregue el primer contador web de prueba

---
+++ Proyectos_Git/Contador/JS.js
@@ -0,0 +1,31 @@
+const numeroFinal = 367;
+const elementoContador = document.getElementById('contador');
+const mensajeFinal = document.getElementById('mensajeFinal');
+
+let numeroInicial = 0;
+
+const velocidadNormal = 10;
+const velocidadLenta = 100;
+const umbralDesaceleracion = 10;
+
+function iniciarContador() {
+  const intervalo = setInterval(() => {
+    numeroInicial++;
+    elementoContador.textContent = numeroInicial;
+
+    if (numeroInicial >= numeroFinal - umbralDesaceleracion) {
+      clearInterval(intervalo);
+      const intervaloLento = setInterval(() => {
+        numeroInicial++;
+        elementoContador.textContent = numeroInicial;
+
+        if (numeroInicial >= numeroFinal) {
+          clearInterval(intervaloLento);
+          mensajeFinal.style.display = 'block';
+        }
+      }, velocidadLenta);
+    }
+  }, velocidadNormal);
+}
+
+window.onload = iniciarContador;
